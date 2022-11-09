53f10d56a838a30420a86f63c1154dc60e2ecbfc
kamron-hasanzoda <116718755+kamron-hasanzoda@users.noreply.github.com>
2022-11-09 13:20:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+// let names = ['Abbos', 'Valentina', 'Odilbek', 'Shaxzod', 'Xojik', 'Emil', 'Saidbek', 'Morgenshtern', 'Alisher', 'Daler']
+
+// let sorted = names.reduce(
+//     function (a, b) {
+//         return a.length > b.length ? a : b;
+//     }
+// );
+// console.log(sorted);
+let arr = [
+    {
+      userId: 1,
+      id: 1,
+      title: "delectus aut au`tem",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "quis ut nam facilis et officia qui",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "fugiat veniam minus",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 4,
+      title: "et porro tempora",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 5,
+      title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 6,
+      title: "qui ullam ratione quibusdam voluptatem quia omnis",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 7,
+      title: "illo expedita consequatur quia in",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 8,
+      title: "quo adipisci enim quam ut ab",
+      completed: true,
+    },
+    {
+      userId: 1,
+  
+      id: 9,
+      title: "molestiae perspiciatis ipsa",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 10,
+      title: "illo est ratione doloremque quia maiores aut",
+      completed: true,
+    },
+  ];
+  let a = {
+    count: 0,
+    arr: [],
+  };
+  
+  let b = {
+    count: 0,
+    arr: [],
+  };
+  arr.filter((item) => {
+    if (item.completed === false) {
+      b.arr.push(item.completed);
+    } else if (item.completed === true) {
+      a.arr.push(item.completed);
+    }
+  });
+  console.log(a.arr, b.arr);
+  a.count = a.arr.length;
+  console.log(a);
+  b.count = b.arr.length;
+  console.log(b);
